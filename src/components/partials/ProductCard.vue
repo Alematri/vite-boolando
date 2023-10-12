57cac8574449a949df49aa824c416c3160209f0a
Alematri <alessandromatricardi@gmail.com>
2023-10-12 15:50:32 +0200

    [fix] img path

--- src/components/partials/ProductCard.vue
+++ src/components/partials/ProductCard.vue
@@ -13,23 +13,23 @@
     <div class="card">
       <div class="card-image">
         <div class="images">
-          <img :src="`/img/+$(productObj.primaryImage)`">
-          <img class="secondary-image" :src="`/img/+$(productObj.secondaryImage)`">
+          <img :src="`/img/${productObj.primaryImage}`">
+          <img class="secondary-image" :src="`/img/${productObj.secondaryImage}`">
         </div>
             
         <div class="favourite">&hearts;</div>
         <div class="card-badge">
           <span v-if="productObj.discount" class="discount">{{productObj.discount}}</span>
-          <span v-if="productObj.sostenibilita" class="tag">sostenibiltà</span>
+          <span v-if="productObj.sostenibilita" class="tag">Sostenibiltà</span>
         </div>
       </div>
 
       <div class="card-text">
         <span class="brand">{{productObj.marca}}</span>
-        <h3 class="product-name">{{productObj.mdello}}</h3>
+        <h3 class="product-name">{{productObj.modello}}</h3>
         <div class="price">
-          <span>{{productObj.fullPrice}}&euro;</span>
-          <span v-if="productObj.fullPrice">{{productObj.lastPrice}}&euro;</span>
+          <span>{{productObj.lastPrice}}&euro;</span>
+          <span v-if="productObj.fullPrice">{{productObj.fullPrice}}&euro;</span>
         </div>
       </div>
     </div>
